fbb11b80ea84116ee4f3e9b83f3d34d962c782d1
xanderoot <45837051+xanderoot@users.noreply.github.com>
2023-01-01 14:01:14 -0800

    Update recipeBot.py

--- recipeBot.py
+++ recipeBot.py
@@ -22,10 +22,10 @@ confirmedItems = ['Summon Skeletons']
 items = ['Elemental Army','Bone Ring','Calling Wand','+1 Minion Helm','+2 Minion Helm','+3 Minion Helm','Vaal Summon Skeletons','The Dark Mage (Div)','The Brawny Battle Mage (Div)',
         'Necromancer Silks (Normal)','Necromancer Silks (Magic)','Necromancer Silks (Rare)','Feeding Frenzy','Meat Shield','Bone Helmet','Elder Influenced Amulet with +1 Maximum Skeletons.',
         'Added Cold Damage Support','Added Fire Damage Support','Added Lightning Damage Support','Tailoring Orb','Lab Enchanted Minion Helmet Damage 25%','Lab Enchanted Minion Helmet Damage 40%',
-        'Faster Casting Support','Spell Echo Support','Cobalt Jewel (Normal)','Cobalt Jewel (Magic)','Cobalt Jewel (Rare)','Cobalt Jewel (Unique)','Chance Orb','Fusing Orb','Ancient Orb',
-        'Herald of Ice','Herald of Thunder','Herald of Ash','Minion Resist Flask','Minion Life Flask','Bone Ring with Resists','Queens Decree','Midnight Bargain','Alberons Warpath',
-        'Lab Enchanted Minion Helmet Damage 20% Chance to Summon Additional','Lab Enchanted Minion Helmet 40% Chance to Summon Additional','From Dust','To Dust','Earendels Embrace',
-        'Femurs of the Saints','The Iron Mass']
+        'Faster Casting Support','Spell Echo Support','Cobalt Jewel All Rarities','Chance Orb','Fusing Orb','Ancient Orb','Herald of Ice','Herald of Thunder','Herald of Ash','Minion Resist Flask',
+        'Minion Life Flask','Bone Ring with Resists','Queens Decree','Midnight Bargain','Alberons Warpath','Lab Enchanted Minion Helmet Damage 20% Chance to Summon Additional',
+        'Lab Enchanted Minion Helmet 40% Chance to Summon Additional','From Dust','To Dust','Earendels Embrace','Femurs of the Saints','The Iron Mass','Trinity Support','Faster Projectiles',
+        'Volley Support','Greater Volley Support','Animate Weapon','Animate Guardian']
 itemsToOutput = []
 
 ##################### Running Code
